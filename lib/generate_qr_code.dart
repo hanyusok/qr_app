2e87ff58495fa651e4de40dcff0f2c69d0985af6
hanyusok <hanyusok@gmail.com>
2024-02-20 00:41:07 +0900

    fixed

--- lib/generate_qr_code.dart
+++ lib/generate_qr_code.dart
@@ -23,17 +23,20 @@ class _GenerateQrCodeState extends State<GenerateQrCode> {
   bool dirExists = false;
   dynamic externalDir = '/storage/emulated/0/Download/Qr_code';
 
-
   /* png이미지 생성 및 저장하기 */
   Future<void> captureAndSavePng() async {
-    try{
-      RenderRepaintBoundary boundary = _qrKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+    try {
+      RenderRepaintBoundary boundary =
+          _qrKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
       final image = await boundary.toImage(pixelRatio: 3.0);
       /* drawing white backgorund because QR code is Black */
       final whitePaint = Paint()..color = Colors.white;
       final recorder = PictureRecorder();
-      final canvas = Canvas(recorder, Rect.fromLTWH(0, 0, image.width.toDouble(), image.height.toDouble()));
-      canvas.drawRect(Rect.fromLTWH(0, 0, image.width.toDouble(), image.height.toDouble()), whitePaint);
+      final canvas = Canvas(recorder,
+          Rect.fromLTWH(0, 0, image.width.toDouble(), image.height.toDouble()));
+      canvas.drawRect(
+          Rect.fromLTWH(0, 0, image.width.toDouble(), image.height.toDouble()),
+          whitePaint);
       canvas.drawImage(image, Offset.zero, Paint());
       final picture = recorder.endRecording();
       final img = await picture.toImage(image.width, image.height);
@@ -42,14 +45,14 @@ class _GenerateQrCodeState extends State<GenerateQrCode> {
       /* file 이름 및 폴더 중복 체크*/
       String fileName = 'qr_code';
       int i = 1;
-      while(await File('$externalDir/$fileName').exists()){
+      while (await File('$externalDir/$fileName').exists()) {
         fileName = 'qr_code_$i';
         i++;
       }
       /* dir 중복 확인 */
       dirExists = await File(externalDir).exists();
       /* dir 없으면 new dir 생성 */
-      if(!dirExists){
+      if (!dirExists) {
         await Directory(externalDir).create(recursive: true);
         dirExists = true;
       }
@@ -57,13 +60,13 @@ class _GenerateQrCodeState extends State<GenerateQrCode> {
       final file = await File('$externalDir/$fileName.png').create();
       await file.writeAsBytes(pngBytes);
       /* 이미지 생성 snackbar 알림 */
-      if(!mounted) return;
+      if (!mounted) return;
       const customSnackBar = SnackBar(content: Text('QR코드가 gallery에 저장되었습니다'));
       ScaffoldMessenger.of(context).showSnackBar(customSnackBar);
-    } catch(e){
+    } catch (e) {
       log(e.toString());
       /* 이미지 생성 에러 snackbar 알림 */
-      if(!mounted) return;
+      if (!mounted) return;
       const customSnackBar = SnackBar(content: Text('QR코드가 gallery에 저장실패!'));
       ScaffoldMessenger.of(context).showSnackBar(customSnackBar);
     }
@@ -73,15 +76,24 @@ class _GenerateQrCodeState extends State<GenerateQrCode> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('QR생성'), centerTitle: true,),
+      appBar: AppBar(
+        title: const Text('QR생성'),
+        centerTitle: true,
+      ),
       body: Center(
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               /* QR 생성된 이미지 */
-              if(urlController.text.isNotEmpty) QrImageView(data: data, size: 200,),
-              const SizedBox(height: 18,),
+              if (urlController.text.isNotEmpty)
+                QrImageView(
+                  data: data,
+                  size: 200,
+                ),
+              const SizedBox(
+                height: 18,
+              ),
               /* QR 컨텐츠 String */
               Container(
                 padding: const EdgeInsets.only(left: 10, right: 10),
@@ -90,27 +102,34 @@ class _GenerateQrCodeState extends State<GenerateQrCode> {
                   controller: urlController,
                   decoration: InputDecoration(
                     contentPadding: const EdgeInsets.all(10),
-                    hintText:  '입력하면 QR코드 생성됩니다.',
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+                    hintText: '입력하면 QR코드 생성됩니다.',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15)),
                     labelText: '입력하기',
                   ),
-                ),                
+                ),
               ),
-              const SizedBox(height: 18,),
+              const SizedBox(
+                height: 18,
+              ),
               /* 생성하기 button */
-              ElevatedButton(onPressed: () {
-                setState(() {
-                  data = urlController.text;
-                });
-              },
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    data = urlController.text;
+                  });
+                },
+                style: CustomStyle().myBtnStyle,
                 child: const Text('생성'),
-                style: CustomStyle().myBtnStyle,
               ),
-              const SizedBox(height: 18,),
+              const SizedBox(
+                height: 18,
+              ),
               /* 저장하기 button */
-              ElevatedButton(onPressed: captureAndSavePng,
-                child: const Text('저장'),
+              ElevatedButton(
+                onPressed: captureAndSavePng,
                 style: CustomStyle().myBtnStyle,
+                child: const Text('저장'),
               ),
             ],
           ),
--- lib/scan_qr_code.dart
+++ lib/scan_qr_code.dart
@@ -13,31 +13,46 @@ class ScanQrCode extends StatefulWidget {
 class _ScanQrCodeState extends State<ScanQrCode> {
   String qrResult = 'Scanned Data will appear here';
   Future<void> scanQR() async {
-    try{
-      final qrCode = await FlutterBarcodeScanner.scanBarcode('#ff6666', '취소', true, ScanMode.QR);
-      if(!mounted) return;
+    try {
+      final qrCode = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', '취소', true, ScanMode.QR);
+      if (!mounted) return;
       setState(() {
         qrResult = qrCode.toString();
       });
       //
-    } on PlatformException{
+    } on PlatformException {
       qrResult = 'QR 읽기 실패!';
       //
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('QR 스캔하기'),) ,
+      appBar: AppBar(
+        title: const Text('QR 스캔하기'),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const SizedBox(height: 30,),
-            Text(qrResult, style: const TextStyle(color: Colors.black),),
-            const SizedBox(height: 30,),
-            ElevatedButton(onPressed: scanQR, child: const Text('스캔하기'), style: CustomStyle().myBtnStyle,),
-            
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              qrResult,
+              style: const TextStyle(color: Colors.black),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: scanQR,
+              style: CustomStyle().myBtnStyle,
+              child: const Text('스캔하기'),
+            ),
+
             //
           ],
         ),
